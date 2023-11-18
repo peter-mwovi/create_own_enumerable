8a0805b5dbe28d72a11ae1433502bdb21ddeff53
Erwin Amador <eamador0305@gmail.com>
2023-11-18 08:28:34 -0600

    correct linter errors

--- my_list.rb
+++ my_list.rb
@@ -1,8 +1,10 @@
-require_relative 'my_enumerable'
+class MyList
+  require_relative 'my_enumerable'
   include MyEnumerable
   def initialize(*args)
     @list = args
   end
+
   def each(&block)
     @list.each { |element| block.call(element) }
   end
