2178b73e314813aa9eefca99b7e049f31602aca8
Peter Mwovi <petermmwovi@gmail.com>
2023-11-18 16:39:01 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -1,13 +1,1 @@
-class MyList
-  require_relative 'my_enumerable'
 
-  include MyEnumerable
-
-  def initialize(*args)
-    @list = args
-  end
-
-  def each(&block)
-    @list.each { |element| block.call(element) }
-  end
-end
